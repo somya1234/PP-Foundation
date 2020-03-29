bcceca35787e4d8ecf45f1ddea3de55e6ac9db54
Somya <somya1234@gmail.com>
2020-03-29 18:50:19 +0530

    start

---
+++ day9/Practice/Recursion/increasing.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class increasing{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        printInc(n);
+    }
+    public static void printInc(int n){
+        if(n==0){
+            return;
+        }
+        printInc(n-1);
+        System.out.println(n);
+    }
+}
